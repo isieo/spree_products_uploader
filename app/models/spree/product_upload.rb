4affa8774a3f51207ca80afedade48b4aa47d245
Calvin Tee <isieox+github@gmail.com>
2016-10-05 18:36:05 +0800

    Update product_upload.rb

--- app/models/spree/product_upload.rb
+++ app/models/spree/product_upload.rb
@@ -43,7 +43,7 @@ module Spree
           if variant_is_master
             if v.first.product.variants.count > 0
               v.first.delete
-              v.first.product.variant.first.update(:master, true) if 
+              v.first.product.variant.first.update(:master, true) 
             else
               v.first.product.delete
             end
